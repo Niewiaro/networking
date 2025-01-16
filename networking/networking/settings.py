4a3376e8378ba29de7416618265f6b6a689cfd60
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-01-17 00:43:17 +0100

    homepage with bootstrap5 & bootstrap icons

--- networking/networking/settings.py
+++ networking/networking/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "main.py",
+    "main",
+    "bootstrap5",
+    "django_bootstrap_icons",
 ]
 
 MIDDLEWARE = [
