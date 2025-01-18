76d5ff51e8b25696df9abf05597bb881e2c60f4b
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-01-18 16:40:27 +0100

    feat: register

--- networking/main/urls.py
+++ networking/main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import homepage
 
 
+app_name = "main"
+
 urlpatterns = [
-    path("", homepage, name="homepage.html"),
+    path("", homepage, name="homepage"),
 ]
