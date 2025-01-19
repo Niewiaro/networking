fec4db28a51a3874a64eb5130a549602a0ddd2ae
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-01-19 15:29:41 +0100

    feat: card page with uuid & slug

--- networking/card/urls.py
+++ networking/card/urls.py
@@ -6,5 +6,7 @@ app_name = "card"
 
 urlpatterns = [
     path("", views.cards_list, name="list"),
+    path("<uuid:identifier>/", views.card_page, name="page_by_id"),
+    path("<slug:identifier>/", views.card_page, name="page_by_slug"),
     path("new", views.card_new, name="new"),
 ]
--- networking/card/views.py
+++ networking/card/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BusinessCard
 from django.contrib.auth.decorators import login_required
+import uuid
 
 
 @login_required(login_url="/users/login/")
@@ -12,3 +13,15 @@ def cards_list(request):
 @login_required(login_url="/users/login/")
 def card_new(request):
     return render(request, "card/card_new.html")
+
+
+@login_required(login_url="/users/login/")
+def card_page(request, identifier):
+    if isinstance(identifier, uuid.UUID):  # Sprawdzenie czy to UUID
+        card = get_object_or_404(BusinessCard, id=identifier)
+    else:  # W przeciwnym razie traktujemy jako slug
+        card = get_object_or_404(BusinessCard, slug=identifier)
+
+    from django.http import HttpResponse
+
+    return HttpResponse(identifier)
